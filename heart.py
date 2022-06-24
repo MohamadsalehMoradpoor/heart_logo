784c216753c79ba184e61526107f759188725785
MohamadsalehMoradpoor <mohammadsalehmoradpoor@gmail.com>
2022-06-24 14:58:33 +0430

    first commit

---
+++ heart.py
@@ -0,0 +1,13 @@
+from turtle import *
+pencolor("pink")
+fillcolor("red")
+begin_fill()
+speed(1)
+pensize(8)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
